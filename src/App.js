3f8ae802c440d0591da35a8aa2cee8efeb2bf03a
Patrik Tomek <pataz23@seznam.cz>
2023-01-09 19:00:32 +0100

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import MainBtn from "./components/MainBtn";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="home">
+      <div className="home_content-container">
+        <div className="home_text_container">
+          <h5>So, you want to travel to</h5>
+          <h1>Space</h1>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <MainBtn />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/MainBtn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainBtn = () => {
+  return (
+    <div>
+      <button className="lp-main_btn">Explore</button>
+    </div>
+  );
+};
+
+export default MainBtn;
