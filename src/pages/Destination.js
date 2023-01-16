1b7686dcd536ff1e84a6606d516156284d755da8
Patrik Tomek <pataz23@seznam.cz>
2023-01-16 19:21:34 +0100

    Destination page data switching

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../data.json";
-import moon from "../assets/destination/image-moon.png";
-import DestinationNav from "../components/DestinationNav";
 import Navigation from "../components/Navigation";
 
 const Destination = () => {
+  const [destinationData, setDestinationData] = useState(data.destinations[0]);
+
   const moonData = data.destinations[0];
-  console.log(moonData.images.png)
+  const marsData = data.destinations[1];
+  const europaData = data.destinations[2];
+  const titanData = data.destinations[3];
+
+  const handleMoonData = () => {
+    setDestinationData(moonData);
+  };
+  const handleMarsData = () => {
+    setDestinationData(marsData);
+  };
+  const handleEuropaData = () => {
+    setDestinationData(europaData);
+  };
+  const handleTitanData = () => {
+    setDestinationData(titanData);
+  };
 
   return (
     <div className="container">
@@ -15,18 +30,36 @@ const Destination = () => {
         <h6 className="page_header">
           <span className="page-num_header">01 </span>Pick your destination
         </h6>
-        <img src={moon} alt="Moon" className="moon_img planet_img" />
+        <img src={destinationData.images.png} alt="Moon" className="moon_img planet_img" />
         <div className="destination-moon__text_container planet__text_container">
           <ul className="destination-nav">
-            <li className="destination-nav_item nav_item destination-nav_item__active">
+            <li
+              onClick={handleMoonData}
+              className="destination-nav_item nav_item destination-nav_item__active"
+            >
               Moon
             </li>
-            <li className="destination-nav_item nav_item">Mars</li>
-            <li className="destination-nav_item nav_item">Europa</li>
-            <li className="destination-nav_item nav_item">Titan</li>
+            <li
+              onClick={handleMarsData}
+              className="destination-nav_item nav_item"
+            >
+              Mars
+            </li>
+            <li
+              onClick={handleEuropaData}
+              className="destination-nav_item nav_item"
+            >
+              Europa
+            </li>
+            <li
+              onClick={handleTitanData}
+              className="destination-nav_item nav_item"
+            >
+              Titan
+            </li>
           </ul>
-          <h2 className="planet_header">{moonData.name}</h2>
-          <p className="p-text p_planet">{moonData.description}</p>
+          <h2 className="planet_header">{destinationData.name}</h2>
+          <p className="p-text p_planet">{destinationData.description}</p>
           <hr className="planet_text_line" />
           <div className="planet-details">
             <div className="planet-details_containers planet-distance">
@@ -34,14 +67,16 @@ const Destination = () => {
                 Avg. distance
               </h6>
               <h6 className="planet-details_info sub-h1">
-                {moonData.distance}
+                {destinationData.distance}
               </h6>
             </div>
             <div className="planet-details_containers planet-travel-time">
               <h6 className="planet-details_sub-header sub-h2">
                 Est. travel time
               </h6>
-              <h6 className="planet-details_info sub-h1">{moonData.travel}</h6>
+              <h6 className="planet-details_info sub-h1">
+                {destinationData.travel}
+              </h6>
             </div>
           </div>
         </div>
