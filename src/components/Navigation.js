04577ba86d9f09c6972f84d0395cb88427f0c4a9
Patrik Tomek <pataz23@seznam.cz>
2023-01-23 15:47:02 +0100

    MainNav for Mobile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,10 +2,22 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
+  const handleMobileNav = (e) => {
+    e.currentTarget.parentNode.lastChild.classList.toggle("open");
+  };
+  const handleClosingMobileNav = (e) => {
+    e.currentTarget.parentNode.classList.toggle("open");
+  };
+
   return (
     <div className="nav_container">
       <div className="logo">
-        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="auto" viewBox="0 0 50 50">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          height="100%"
+          viewBox="0 0 50 50"
+        >
           <g fill="none" fillRule="evenodd">
             <circle cx="24" cy="24" r="24" fill="#FFF" />
             <path
@@ -16,16 +28,24 @@ const Navigation = () => {
         </svg>
       </div>
       <div className="nav-line"></div>
-      <div className="nav_hamburger">
+      <div onClick={handleMobileNav} className="nav_hamburger">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21">
           <g fill="#D0D6F9" fillRule="evenodd">
             <path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" />
           </g>
         </svg>
       </div>
-      <nav className="main-nav">
+      <nav className="main-nav main-nav_mobile">
+        <div onClick={handleClosingMobileNav} className="nav_close">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21">
+            <g fill="#D0D6F9" fillRule="evenodd">
+              <path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z" />
+              <path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z" />
+            </g>
+          </svg>
+        </div>
         <ul className="main-nav_list">
-          <NavLink to="/space-tourism-website-react/">
+          <NavLink to="/space-tourism-website-react/" className="main-nav_link">
             <li className="main-nav-home main-nav_item nav_item">
               <span className="main-nav_span">00</span>Home
             </li>
