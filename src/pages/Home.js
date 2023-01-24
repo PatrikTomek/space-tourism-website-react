f614962eda450081af0b2e0a1dc28a05009948db
Patrik Tomek <pataz23@seznam.cz>
2023-01-24 18:30:45 +0100

    Attribution

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,21 +4,29 @@ import Navigation from "../components/Navigation";
 
 const Home = () => {
   return (
-    <div className="home_full-container container">
-      <div className="home page_content-container">
-        <Navigation />
-        <div className="home_text_container">
-          <h5>So, you want to travel to</h5>
-          <h1>Space</h1>
-          <p>
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience!
-          </p>
+    <div>
+      {" "}
+      <div className="home_full-container container">
+        <div className="home page_content-container">
+          <Navigation />
+          <div className="home_text_container">
+            <h5>So, you want to travel to</h5>
+            <h1>Space</h1>
+            <p>
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+          <MainBtn />
         </div>
-        <MainBtn />
       </div>
+      <p className="attribution">
+        Challenge by
+        <a href="https://www.frontendmentor.io/home"> Frontend Mentor.</a> Coded
+        by <a href="https://patriktomek.cz/">Patrik Tomek</a>
+      </p>
     </div>
   );
 };
